7e00f8063d2bc3f3a8c0ddbafdd7d813c5f819d4
Hong JUN PYO <wnsvy4231@naver.com>
2021-11-18 15:43:40 +0900

    테스트

--- react_notice_board/src/App.js
+++ react_notice_board/src/App.js
@@ -4,15 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
-      <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-        <button type="button" class="btn btn-danger">Left</button>
-        <button type="button" class="btn btn-warning">Middle</button>
-        <button type="button" class="btn btn-success">Right</button>
-      </div>
-  
+        <button type="button" class="btn btn-danger">danger</button>
     </div>
   );
 }
 
 export default App;
+
