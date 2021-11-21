833427c88bb0d3c23bd075363a6e17d017d1262e
Hong JUN PYO <wnsvy4231@naver.com>
2021-11-21 20:50:16 +0900

    Update App.js

--- react_notice_board/src/App.js
+++ react_notice_board/src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
         <button type="button" class="btn btn-danger">danger</button>
+        <button type="button" class="btn btn-warning">warning</button>
+        <button type="button" class="btn btn-success">success</button>
     </div>
   );
 }
